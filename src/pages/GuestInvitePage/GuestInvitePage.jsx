b1c2a3fcb861247a18357bcdc64e713960497224
Andrewbui389 <andrewbui389@gmail.com>
2022-09-12 01:32:00 -0400

    fix render

--- src/pages/GuestInvitePage/GuestInvitePage.jsx
+++ src/pages/GuestInvitePage/GuestInvitePage.jsx
@@ -1,7 +1,7 @@
-import GuestQRCode from "../../components/GuestQRCode/GuestQRCode";
 import './GuestInvitePage.css'
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import GuestQRCode from '../../components/GuestQRCode/GuestQRCode';
 
 export default function GuestInvitePage({changPageStat}){
     useEffect(() => {
@@ -9,6 +9,7 @@ export default function GuestInvitePage({changPageStat}){
     })
     
     const { eventId , guestId } = useParams()
+
     return(
     <div className="qrCode">
         <GuestQRCode url={`/updateguest/checkin/${eventId}/${guestId}`}/>
